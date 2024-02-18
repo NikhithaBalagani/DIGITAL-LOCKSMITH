c54b23c3e362bcc0cfd1c3881f0c357919a27bb4
Nikhitha Balagani <40018198+NikhithaBalagani@users.noreply.github.com>
2024-02-18 16:58:56 -0500

    Add files via upload

---
+++ home/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+from . import views
+from .views import generate_password
+
+urlpatterns = [
+    path('', views.user_login_view, name='index'),
+    path('register/', views.register_page, name='register-page'),
+    path('home/', views.home_page, name='home'),
+    path('logout/', views.logout_view, name="logout"),
+
+    #  user passwords
+    path('add-password/', views.add_new_password, name="add-password"),
+    path('manage-passwords/', views.manage_passwords, name="manage-passwords"),
+    path('edit-password/<str:pk>/', views.edit_password, name="edit-password"),
+    path('search/', views.search, name='search'),
+
+
+    path('generate-password/', generate_password, name='generate-password'),
+]
